7a1901907d5b793c8d45aa03177ee5c95d191bff
AofPatiphan <patiphan.ma@hotmail.com>
2022-02-23 13:52:37 +0700

    save

--- app.js
+++ app.js
@@ -20,7 +20,7 @@ const cors = require('cors');
 //     credentials: true, //access-control-allow-credentials:true
 //     optionSuccessStatus: 200,
 // };
-// const app = express();
+const app = express();
 
 app.use(cors());
 app.use(express.json({ limit: '50mb' }));
