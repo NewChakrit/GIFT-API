9b5b6f5fb30ef4db9c659b17814e4c68453347b3
AofPatiphan <patiphan.ma@hotmail.com>
2022-02-23 13:51:36 +0700

    save

--- app.js
+++ app.js
@@ -15,14 +15,14 @@ require('dotenv').config();
 require('./middlewares/passport');
 
 const cors = require('cors');
-const corsOptions = {
-    origin: '*',
-    credentials: true, //access-control-allow-credentials:true
-    optionSuccessStatus: 200,
-};
-const app = express();
+// const corsOptions = {
+//     origin: '*',
+//     credentials: true, //access-control-allow-credentials:true
+//     optionSuccessStatus: 200,
+// };
+// const app = express();
 
-app.use(cors(corsOptions));
+app.use(cors());
 app.use(express.json({ limit: '50mb' }));
 app.use(
     express.urlencoded({
